8b77010ffa5f569c34dbb982eceb87cdd462d8de
whoover <whoover@nemours.org>
2020-04-29 09:24:11 -0700

    DB usage

--- mssql/install.sh
+++ mssql/install.sh
@@ -68,11 +68,11 @@ if [[ "${P_UID}" != "sa" ]]; then
   echo "Creating MSSQL database: ${P_DDB}"
   sqlcmd -S localhost -U sa -P $MSSQL_SA_PWD -Q "CREATE DATABASE ${P_UID}"
   echo "Creating MSSQL login $P_UID with password authentication"
-  sqlcmd -S localhost -U sa -P $MSSQL_SA_PWD -Q "CREATE LOGIN ${P_UID} WITH PASSWORD = '${P_PWD}'"
+  sqlcmd -S localhost -U sa -P $MSSQL_SA_PWD -Q "USE ${P_UID}; CREATE LOGIN ${P_UID} WITH PASSWORD = '${P_PWD}';"
   echo "Creating MSSQL user for login $P_UID on schema ${P_DDB}"
-  sqlcmd -S localhost -U sa -P $MSSQL_SA_PWD -Q "CREATE USER ${P_UID} FOR LOGIN ${P_UID} WITH DEFAULT_SCHEMA = ${P_DDB}"
+  sqlcmd -S localhost -U sa -P $MSSQL_SA_PWD -Q "USE ${P_UID}; CREATE USER ${P_UID} FOR LOGIN ${P_UID} WITH DEFAULT_SCHEMA = ${P_DDB};"
   echo "Granting MSSQL user $P_UID all permissions to ${P_DDB}"
-  sqlcmd -S localhost -U sa -P $MSSQL_SA_PWD -Q "GRANT ALL ON ${P_UID} TO ${P_UID}"
+  sqlcmd -S localhost -U sa -P $MSSQL_SA_PWD -Q "USE ${P_UID}; GRANT ALL ON ${P_UID};"
   # test connection
   sqlcmd -S localhost -U "${P_UID}" -P "${P_PWD}" -Q "SELECT 1"
 else
