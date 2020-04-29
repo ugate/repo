b9e85c088ae5f4d2358ce53e2a17e90e72d72e99
whoover <whoover@nemours.org>
2020-04-29 07:55:00 -0700

    dpkg configure

--- mssql/install.sh
+++ mssql/install.sh
@@ -40,6 +40,8 @@ sudo MSSQL_PID=Developer ACCEPT_EULA=Y MSSQL_SA_PASSWORD="${MSSQL_SA_PWD}" /opt/
 
 echo "Installing MSSQL Tools"
 
+sudo dpkg --configure -a
+
 # register microsoft repository
 curl https://packages.microsoft.com/config/ubuntu/${MSSQL_UBUNTU_VER}/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
 
